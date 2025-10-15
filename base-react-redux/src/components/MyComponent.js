1867fd2bb5a94d4658978f0e70c8c6afff6b88b0
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-10-16 01:01:51 +0700

    learn about state

--- base-react-redux/src/components/MyComponent.js
+++ base-react-redux/src/components/MyComponent.js
@@ -1,9 +1,18 @@
 import { Component } from "react";
 
 class MyComponent extends Component {
+
+    state = {
+        name: 'Hung',
+        address: 'Nha Trang',
+        age: 22
+    };
+
     render(){
         return (
-            <div>My First Component</div>
+            <div>
+                My name is {this.state.name} and I'm from {this.state.address}
+            </div>
         );
     }
 }
