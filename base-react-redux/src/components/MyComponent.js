88e6be4b0428cb45b7f142257e5d35c42cd61f35
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-10-16 00:48:59 +0700

    init first component

---
+++ base-react-redux/src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class MyComponent extends Component {
+    render(){
+        return (
+            <div>My First Component</div>
+        );
+    }
+}
+
+export default MyComponent;
