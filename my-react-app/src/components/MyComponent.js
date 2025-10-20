e686a90f765fc983e4b964ddbb32ea7c32553706
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-10-20 19:25:52 +0700

    DOM Event with React

--- my-react-app/src/components/MyComponent.js
+++ my-react-app/src/components/MyComponent.js
@@ -4,11 +4,21 @@ class MyComponent extends Component {
 
     state = {
         name: "Hung Nguyen"
-    };
+    }
+
+    handleClickButton = () => {
+        console.log(`Thank you for clicked this button <3`);
+    }
 
     render() {
         return (
-            <div>Hello World. My name is {this.state.name}</div>
+            <div>
+                <span>Hello World. My name is {this.state.name}</span>
+
+                <div>
+                    <button onClick={this.handleClickButton}>Click me!</button>
+                </div>
+            </div>
         )
     }
 }
