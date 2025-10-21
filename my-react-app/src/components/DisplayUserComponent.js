e704f0f22752c4029bc25d3a547d1eabb13050f7
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-10-22 00:49:35 +0700

    Update DisplayUserComponent.js

--- my-react-app/src/components/DisplayUserComponent.js
+++ my-react-app/src/components/DisplayUserComponent.js
@@ -1,58 +1,111 @@
-import { Component } from "react";
+// import { Component } from "react";
 
-class DisplayUserComponent extends Component {
-    handleDelete = (index) => {
+// class DisplayUserComponent extends Component {
+//     handleDelete = (index) => {
+//         if (window.confirm("Are you sure you want to delete this user?")) {
+//             this.props.handleDeleteUser(index);
+//         }
+//     }
+
+//     render() {
+//         const { users } = this.props;
+
+//         return (
+//             <div className="table-container">
+//                 <h4>List users</h4>
+//                 <table border="1" cellPadding="5" style={{ borderCollapse: "collapse", width: "100%" }}>
+//                     <thead>
+//                         <tr>
+//                             <th>#</th>
+//                             <th>Name</th>
+//                             <th>Email</th>
+//                             <th>Phone</th>
+//                             <th>Action</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         {
+//                             users && users.length > 0 ? (
+//                                 users.map((user, index) => (
+//                                     <tr key={index}>
+//                                         <td>{index + 1}</td>
+//                                         <td>{user.name}</td>
+//                                         <td>{user.email}</td>
+//                                         <td>{user.phone}</td>
+//                                         <td>
+//                                             <button
+//                                                 onClick={() => this.handleDelete(index)}
+//                                                 style={{ backgroundColor: "red", color: "white", border: "none", padding: "5px 10px", cursor: "pointer" }}
+//                                             >
+//                                                 Delete
+//                                             </button>
+//                                         </td>
+//                                     </tr>
+//                                 ))
+//                             ) : (
+//                                 <tr>
+//                                     <td colSpan="5" style={{ textAlign: "center" }}>No data</td>
+//                                 </tr>
+//                             )
+//                         }
+//                     </tbody>
+//                 </table>
+//             </div>
+//         );
+//     }
+// }
+
+const DisplayUser = (props) => {
+    const { users } = props;
+
+    const handleDelete = (index) => {
         if (window.confirm("Are you sure you want to delete this user?")) {
-            this.props.handleDeleteUser(index);
+            props.handleDeleteUser(index);
         }
     }
 
-    render() {
-        const { users } = this.props;
-
-        return (
-            <div className="table-container">
-                <h4>List users</h4>
-                <table border="1" cellPadding="5" style={{ borderCollapse: "collapse", width: "100%" }}>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users && users.length > 0 ? (
-                                users.map((user, index) => (
-                                    <tr key={index}>
-                                        <td>{index + 1}</td>
-                                        <td>{user.name}</td>
-                                        <td>{user.email}</td>
-                                        <td>{user.phone}</td>
-                                        <td>
-                                            <button
-                                                onClick={() => this.handleDelete(index)}
-                                                style={{ backgroundColor: "red", color: "white", border: "none", padding: "5px 10px", cursor: "pointer" }}
-                                            >
-                                                Delete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))
-                            ) : (
-                                <tr>
-                                    <td colSpan="5" style={{ textAlign: "center" }}>No data</td>
+    return (
+        <div className="table-container">
+            <h4>List users</h4>
+            <table border="1" cellPadding="5" style={{ borderCollapse: "collapse", width: "100%" }}>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users && users.length > 0 ? (
+                            users.map((user, index) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.phone}</td>
+                                    <td>
+                                        <button
+                                            onClick={() => handleDelete(index)}
+                                            style={{ backgroundColor: "red", color: "white", border: "none", padding: "5px 10px", cursor: "pointer" }}
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
                                 </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan="5" style={{ textAlign: "center" }}>No data</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    );    
 }
 
-export default DisplayUserComponent;
+export default DisplayUser;
