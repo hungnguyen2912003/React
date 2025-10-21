2c7bfded9dc340255bf7a5b50b75b4b3e2fc6cff
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-10-21 19:51:37 +0700

    hello world with reactjs

---
+++ my-react-app/src/components/MainComponent.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class MainComponent extends Component {
+    render() {
+        return (
+            <div>Hello World</div>
+        )
+    }
+}
+
+export default MainComponent
