364149133c6f799146e76b0d6cf808fc49f25ffc
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-10-21 20:03:55 +0700

    DOM Event

--- my-react-app/src/components/MainComponent.js
+++ my-react-app/src/components/MainComponent.js
@@ -6,11 +6,22 @@ class MainComponent extends Component {
         name: 'Hưng Nguyễn'
     }
 
+    //DOM Event
+    handleCLick = () => {
+        console.log(`You clicked this button!`);
+    }
+
     render() {
         return (
-            <div>
-                <h4>My name is {this.state.name}</h4>
-            </div>
+            <>
+                <div>
+                    <h4>My name is {this.state.name}</h4>
+                </div>
+                
+                <div>
+                    <button onClick={this.handleCLick}>Click me</button>
+                </div>
+            </>
         )
     }
 }
