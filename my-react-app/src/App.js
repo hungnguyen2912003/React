d8737131a81fa0677c0d2be0b579d27b03ce9a1b
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2025-10-22 13:08:49 +0700

    del

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import MainComponent from './components/MainComponent';
 
 function App() {
   return (
     <div className="App">
-      <MainComponent/>
+      
     </div>
   );
 }
